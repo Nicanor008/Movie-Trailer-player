dcbea977d03b3bc3fd2c47756cba0215cc33ef6a
nICANOR <nicanorkorir008@gmail.com>
2018-07-12 13:51:35 +0300

    Images added

--- entertainment_center.py
+++ entertainment_center.py
@@ -16,9 +16,9 @@ spiderman = media.Movie("SpiderMan",
                      "Spider-Man is a fictional superhero appearing in American comic books published by Marvel Comics. The character was created by writer-editor Stan Lee and writer-artist Steve Ditko, and first appeared",
                      "https://upload.wikimedia.org/wikipedia/commons/9/95/Spider-Man2.jpg",
                      "https://www.youtube.com/watch?v=U0D3AOldjMU")
-Moana = media.Movie("Moana",
-                    "An adventurous teenager sails out on a daring mission to save her people. During her journey, Moana meets the once-mighty demigod Maui, who guides her in her quest to become a master way-finder. Together they sail across the open ocean on an action-packed voyage, encountering enormous monsters",
-                    "https://upload.wikimedia.org/wikipedia/commons/1/16/Moana.svg",
-                    "https://www.youtube.com/watch?v=LKFuXETZUsI")
-movies = [toy_story, frozen, spiderman, Moana]
+#Moana = media.Movie("Moana",
+                    #"An adventurous teenager sails out on a daring mission to save her people. During her journey, Moana meets the once-mighty demigod Maui, who guides her in her quest to become a master way-finder. Together they sail across the open ocean on an action-packed voyage, encountering enormous monsters",
+                    #"https://upload.wikimedia.org/wikipedia/commons/1/16/Moana.svg",
+                    #"https://www.youtube.com/watch?v=LKFuXETZUsI")
+movies = [toy_story, frozen, spiderman]
 fresh_tomatoes.open_movies_page(movies)
